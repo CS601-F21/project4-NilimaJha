0e8feab375068db3f731903d0338221d5bef61a3
Nilima Jha <njha2@dons.usfca.edu>
2021-11-24 23:40:08 -0800

    Add more attributes in ClientInfo class to store client information.

--- src/main/java/web/login/ClientInfo.java
+++ src/main/java/web/login/ClientInfo.java
@@ -1,2 +1,48 @@
-package web.login;public class ClientInfo {
+package web.login;
+
+/**
+ * A class to maintain info about each client.
+ */
+public class ClientInfo {
+    private String name;
+    private String email;
+    private String access_token;
+
+    /**
+     * Constructor
+     * @param name
+     */
+    public ClientInfo(String name, String email, String access_token) {
+        this.name = name;
+        this.email = email;
+        this.access_token = access_token;
+    }
+//    public ClientInfo(String name) {
+//        this.name = name;
+//        this.email = email;
+//    }
+
+    /**
+     * return name
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * return email
+     * @return
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * return access_token
+     * @return
+     */
+    public String getAccessToken() {
+        return access_token;
+    }
 }
