401e184e93a9952770fc6c5638d4f5e6a9343a0b
Nilima Jha <njha2@dons.usfca.edu>
2021-11-24 23:56:08 -0800

    Add static block to read config file.

--- src/main/java/web/HttpServer.java
+++ src/main/java/web/HttpServer.java
@@ -1,2 +1,27 @@
-package web;public class HttpServer {
+package web;
+
+import com.google.gson.Gson;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utils.LoginWithSlackConfig;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@SpringBootApplication
+public class HttpServer {
+    public static LoginWithSlackConfig loginWithSlackConfig;
+    static {
+        // read the client id and secret from a config file
+        Gson gson = new Gson();
+        try {
+            loginWithSlackConfig = gson.fromJson(new FileReader("slackConfig.json"), LoginWithSlackConfig.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpServer.class, args);
+    }
 }
