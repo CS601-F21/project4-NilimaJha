9ae0b16b821d22b5de766618121e423f5e8744f3
Nilima Jha <njha2@dons.usfca.edu>
2021-11-28 02:13:00 -0800

    add method to handle post on /completeProfile and finish proper login flow.

--- src/main/java/web/login/LoginController.java
+++ src/main/java/web/login/LoginController.java
@@ -5,15 +5,21 @@ import jdbc.JDBCConnectionPool;
 import model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import web.HttpServer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Map;
+
+import static jdbc.JDBCConnectionPool.updateUserInfoTable;
 
 //import static jdbc.JDBCConnectionPool.executeSelectFromUserInfoWhere;
 
@@ -21,7 +27,7 @@ import java.util.Map;
 public class LoginController {
 
     @GetMapping("/")
-    protected String login (HttpServletRequest req, Model model) throws IOException {
+    protected String login (HttpServletRequest req, Model model) {
         // retrieve the ID of this session from request
         String sessionId = req.getSession(true).getId();
         // determine whether the user is already authenticated
@@ -52,25 +58,26 @@ public class LoginController {
         if (emailId != null) {
             // already authed, no need to log in
             User user = null;
-            try (Connection connection = DBCPDataSource.getConnection()){
+//            try (Connection connection = DBCPDataSource.getConnection()){
+            try {
                 user = JDBCConnectionPool.findUserFromUserInfoByEmailId(emailId);
-                model.addAttribute("userName", user.getUserName());
             } catch(SQLException e) {
-                System.out.println(">>>>>++++++++++++++++++++++++++SQLException+++++++++++++++++++++++++++");
                 e.printStackTrace();
             }
-            return "home";
+            if (user.isCompleteProfile() == true) {
+                model.addAttribute("userName", user.getUserName());
+                return "home";
+            } else {
+                model.addAttribute("user", user);
+                return "completeProfile";
+            }
         } else {
             // retrieve the code provided by Slack
             String code = req.getParameter(LoginServerConstants.CODE_KEY);
             if (req.getParameter(LoginServerConstants.CODE_KEY) == null) {
                 return "redirect:/";
             } else {
-                // generate the url to use to exchange the code for a token:
-                // After the user successfully grants your app permission to access their Slack profile,
-                // they'll be redirected back to your service along with the typical code that signifies
-                // a temporary access code. Exchange that code for a real access token using the
-                // /openid.connect.token method.
+                // generate the url to use to exchange the code for a token using "/openid.connect.token method".
                 String url = LoginUtilities.generateSlackTokenURL(HttpServer.loginWithSlackConfig.getClient_id(),
                         HttpServer.loginWithSlackConfig.getClient_secret(), code,
                         HttpServer.loginWithSlackConfig.getRedirect_url());
@@ -82,13 +89,12 @@ public class LoginController {
                     return "loginUnsuccessful";
                 } else {
                     req.getSession().setAttribute("emailId", user.getUserEmailId());
-                    System.out.println("Login Was Successful and emailId =" + user.getUserEmailId());
                     try {
                         User userFromDB = JDBCConnectionPool.findUserFromUserInfoByEmailId(user.getUserEmailId());
                         if (!userFromDB.isValid()) {
-                            //add user data to db
                             JDBCConnectionPool.executeInsertInUserInfo(user);
-                            model.addAttribute("userName", user.getUserName());
+                            model.addAttribute("user", user);
+                            return "completeProfile";
                         } else {
                             model.addAttribute("userName", userFromDB.getUserName());
                         }
@@ -101,14 +107,40 @@ public class LoginController {
         }
     }
 
+    @PostMapping("/completeProfile")
+    protected String completeProfile(HttpServletRequest req, @Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+        // retrieve the ID of this session
+        String sessionId = req.getSession(true).getId();
+        // retrieve userEmailId associated to this session.
+        String emailId = (String) req.getSession().getAttribute("emailId");
+        System.out.println(">>> USERS EMAIL: " + emailId);
+        if (bindingResult.hasErrors()){
+            System.out.println("*******HAS ERRORS*******");
+            return "completeProfile";
+        }
+        if (emailId != null) {
+            // already authed, no need to log in
+            try {
+                JDBCConnectionPool.updateUserInfoTable(emailId, user);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return "redirect:/home";
+        } else {
+            // delete session if created  before redirect
+            System.out.println("NOT logged in");
+            return "redirect:/";
+        }
+    }
+
     @GetMapping("/logout")
     protected String logout(HttpServletRequest req) throws IOException {
         // retrieve the ID of this session
         String sessionId = req.getSession(true).getId();
         String emailId = (String) req.getSession().getAttribute("emailId");
-        System.out.println(emailId);
         if (emailId != null) {
             // already authed, invalidating session and sending to the login page.
+            System.out.println("Logging out...");
             req.getSession().invalidate();
             return "redirect:/";
         } else {
