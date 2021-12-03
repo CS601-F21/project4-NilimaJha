f1e63e0da33c8466bb33ea04cd03b718555601d1
Nilima Jha <njha2@dons.usfca.edu>
2021-12-03 01:19:14 -0800

    Add class needed for the search by specific keyword or phrase feature.

--- src/main/java/model/EventSearchKeyValue.java
+++ src/main/java/model/EventSearchKeyValue.java
@@ -1,2 +1,34 @@
-package model;public class EventSearchKeyValue {
+package model;
+
+public class EventSearchKeyValue {
+    String searchCategory;
+    String searchType;
+    String searchTerm;
+
+    public EventSearchKeyValue() {
+    }
+
+    public String getSearchCategory() {
+        return searchCategory;
+    }
+
+    public String getSearchType() {
+        return searchType;
+    }
+
+    public String getSearchTerm() {
+        return searchTerm;
+    }
+
+    public void setSearchCategory(String searchCategory) {
+        this.searchCategory = searchCategory;
+    }
+
+    public void setSearchType(String searchType) {
+        this.searchType = searchType;
+    }
+
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
 }
