ab56ee5311e4551f8cb522acfdcd131abd532346
Nilima Jha <njha2@dons.usfca.edu>
2021-12-03 01:12:58 -0800

    Add Ticket class to hold ticket related information from table data.

--- src/main/java/model/Tickets.java
+++ src/main/java/model/Tickets.java
@@ -1,2 +1,61 @@
-package model;public class Tickets {
+package model;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.validation.constraints.Size;
+
+public class Tickets {
+
+    private int ticketId;
+    private int eventId;
+    private String ticketOwnerId;
+    private String purchaseDate;
+    private int totalTicketsSelected;
+
+    public Tickets() {
+    }
+
+    public Tickets(int totalTicketsSelected) {
+        this.totalTicketsSelected = totalTicketsSelected;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public String getTicketOwnerId() {
+        return ticketOwnerId;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public int getTotalTicketsSelected() {
+        return totalTicketsSelected;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setTicketOwnerId(String ticketOwnerId) {
+        this.ticketOwnerId = ticketOwnerId;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public void setTotalTicketsSelected(int totalTicketsSelected) {
+        this.totalTicketsSelected = totalTicketsSelected;
+    }
 }
