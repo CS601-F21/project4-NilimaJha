011d2c1b78689e8ace0665b9bc6823a0daf8b963
Nilima Jha <njha2@dons.usfca.edu>
2021-12-04 00:58:49 -0800

    Add TicketTransfer class to store ticket to be transfered to another user information.

--- src/main/java/model/TicketTransfer.java
+++ src/main/java/model/TicketTransfer.java
@@ -1,2 +1,40 @@
-package model;public class TicketTransfer {
+package model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+
+public class TicketTransfer {
+//    @NotEmpty
+//    @Size(max = 10, min = 1, message = "Mobile number should be of 10 digits")
+//    @Pattern(regexp = "[0-9][0-9]{9}", message = "Mobile number is invalid!!")
+    private ArrayList<Integer> ticketIdList = new ArrayList<>();
+    private String transferee;
+    private String transferor;
+
+    public ArrayList<Integer> getTicketIdList() {
+        return ticketIdList;
+    }
+
+    public String getTransferee() {
+        return transferee;
+    }
+
+    public String getTransferor() {
+        return transferor;
+    }
+
+    public void setTicketIdList(ArrayList<Integer> ticketIdList) {
+        this.ticketIdList = ticketIdList;
+    }
+
+    public void setTransferee(String transferee) {
+        this.transferee = transferee;
+    }
+
+    public void setTransferor(String transferor) {
+        this.transferor = transferor;
+    }
 }
+
