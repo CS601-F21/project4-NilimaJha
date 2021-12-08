2b30cb929af967606fc88968c05443826a9107c1
Nilima Jha <njha2@dons.usfca.edu>
2021-12-08 02:14:16 -0800

    Write customize validation annotation class for transfree field of TicketTransfer class.

--- src/main/java/validation/TransfereeEmail.java
+++ src/main/java/validation/TransfereeEmail.java
@@ -1,2 +1,17 @@
-package validation;public @interface UserList {
+package validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TransfereeEmailValidation.class)
+public @interface TransfereeEmail {
+    String message() default ("User with Given Email Id does not exist. Enter Valid User.");
+    Class<?>[] groups() default {};
+    public abstract Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/validation/TransfereeEmailValidation.java
+++ src/main/java/validation/TransfereeEmailValidation.java
@@ -1,2 +1,31 @@
-package validation;public class TransfereeEmailValidation {
+package validation;
+
+import jdbc.JDBCConnectionPool;
+import model.User;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.sql.SQLException;
+
+public class TransfereeEmailValidation implements ConstraintValidator<TransfereeEmail, String> {
+
+    @Override
+    public void initialize(TransfereeEmail constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        User user = new User();
+        try {
+            user = JDBCConnectionPool.findUserFromUserInfoByEmailId(s);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (user.isValid()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
