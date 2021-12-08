123bc47b4c83248527d569a1fbe68ff5e86c18c6
Nilima Jha <njha2@dons.usfca.edu>
2021-12-08 02:15:59 -0800

    Write customize validation annotation class for ticketIdList field of TicketTransfer class.

--- src/main/java/validation/TransferTicketIdList.java
+++ src/main/java/validation/TransferTicketIdList.java
@@ -1,2 +1,18 @@
-package validation;public @interface TicketIdList {
+package validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TransferTicketIdListValidation.class)
+public @interface TransferTicketIdList {
+//    String message() default "No Ticket Selected.";
+    String message() default ("No Ticket Selected.");
+    Class<?>[] groups() default {};
+    public abstract Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/validation/TransferTicketIdListValidation.java
+++ src/main/java/validation/TransferTicketIdListValidation.java
@@ -1,2 +1,22 @@
-package validation;public class TransferticketIdListValidation {
+package validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+
+public class TransferTicketIdListValidation implements ConstraintValidator<TransferTicketIdList, ArrayList<Integer>> {
+
+    @Override
+    public void initialize(TransferTicketIdList constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(ArrayList<Integer> ticketIdList, ConstraintValidatorContext constraintValidatorContext) {
+        if (ticketIdList.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
 }
