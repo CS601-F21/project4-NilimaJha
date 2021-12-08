75334d5137ebe382cb7548142407157db014d049
Nilima Jha <njha2@dons.usfca.edu>
2021-12-08 02:11:14 -0800

    Write customize validation annotation class for dateOfBirth field of Event class.

--- src/main/java/validation/UserBirthDate.java
+++ src/main/java/validation/UserBirthDate.java
@@ -1,2 +1,18 @@
-package validation;public @interface UserBirthDate {
+package validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UserBirthDateValidation.class)
+public @interface UserBirthDate {
+
+    String message() default("Date should be from past.");
+    Class<?>[] groups() default {};
+    public abstract Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/validation/UserBirthDateValidation.java
+++ src/main/java/validation/UserBirthDateValidation.java
@@ -1,2 +1,28 @@
-package validation;public class UserBirthDateValidation {
+package validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDateTime;
+
+public class UserBirthDateValidation implements ConstraintValidator<UserBirthDate, String> {
+
+    @Override
+    public void initialize(UserBirthDate constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String date, ConstraintValidatorContext constraintValidatorContext) {
+
+        // creating local time from the date provided by user.
+        LocalDateTime inputDateTime = LocalDateTime.parse(date+"T23:59:59.000");
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // comparing date provided by user with the current date.
+        if (inputDateTime.isBefore(currentDateTime)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
