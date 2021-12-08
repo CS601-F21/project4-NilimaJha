f1f83ff07bdfb7b7d5e7401ba7a51f2eb06b2786
Nilima Jha <njha2@dons.usfca.edu>
2021-12-08 02:26:08 -0800

    Remove hardcoded config file name.

--- src/main/java/web/HttpServer.java
+++ src/main/java/web/HttpServer.java
@@ -4,18 +4,21 @@ import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import utils.LoginWithSlackConfig;
+import web.login.LoginServerConstants;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 @SpringBootApplication
 public class HttpServer {
+
     public static LoginWithSlackConfig loginWithSlackConfig;
     static {
         // read the client id and secret from a config file
         Gson gson = new Gson();
         try {
-            loginWithSlackConfig = gson.fromJson(new FileReader("slackConfig.json"), LoginWithSlackConfig.class);
+            loginWithSlackConfig = gson.fromJson(
+                    new FileReader(LoginServerConstants.SLACK_CONFIG_FILE_NAME), LoginWithSlackConfig.class);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
